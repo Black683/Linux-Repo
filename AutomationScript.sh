812dde228b0833695865d13a83255b9f28359ddf
test <test@gmail.com>
2025-02-26 14:39:05 +0000

    testing the functionality

---
+++ AutomationScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Trying to automate the code for basic web APP
+
+$Number1
+$Number2
+
+read -p " Enter one number for the additon opperation: " Number1
+read -p " Enter the second number for the addition opperation: " Number2
+
+echo $Number1
+echo -e " The total value for the addition opperation is: $(( Number1 + Number2)) "
+echo -e "----- End of the Program------"
+
+
